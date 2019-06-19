ce3ea56e94825a4b62242063d39626f0a6e84746
kieran wood <sinister098@gmail.com>
2019-06-18 20:31:56 -0600

    added remaining example files

---
+++ main.py
@@ -0,0 +1,19 @@
+"""
+Your primary file docstring goes here. You can choose any format you like just keep it consistent. 
+
+Here are a few good options:
+    Google Style Guide: https://github.com/google/styleguide/blob/gh-pages/pyguide.md#38-comments-and-docstrings
+    Pep 8: https://www.python.org/dev/peps/pep-0008/#documentation-strings
+    reStructuredText: https://www.python.org/dev/peps/pep-0287/#docstring-significant-features
+"""
+__author__ = ""
+__copyright__ = ""
+__credits__ = []
+__license__ = ""
+__version__ = ""
+__maintainer__ = ""
+__email__ = ""
+__status__ = ""
+
+# This is not ideal, but can be used in a pinch if you're lazy
+from .utilities import *
