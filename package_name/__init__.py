1a9001181266c50e58a9951c3434af9f65d58bce
Kieran Wood <kieranw098@gmail.com>
2020-01-31 10:03:27 -0700

    Updated example module with useful information.

---
+++ package_name/__init__.py
@@ -0,0 +1,14 @@
+"""
+You shouldn't need to modify this file, but if you are making submodules (nested folders + files) you
+    will need to include a __init__.py file that has relative import statements.
+
+Example
+-------
+If you have a submodule folder called /foo, with a module inside called bar.py you will need to include
+    a __init__.py file in /foo with the following import statement:
+        ```
+        from .bar import *
+        ```
+    If you don't do this python won't find the contents of the files inside the submodule folder.
+
+"""
--- package_name/example.py
+++ package_name/example.py
@@ -1,1 +1,34 @@
-"""This file could be imported using package_name.example"""
+"""This is where you should include a module docstring to explain this file's functionality.
+
+There are various formats SEE: https://canadiancoding.ca/posts/post/python/docstrings/
+    For this example I will use numpystyle docstrings: https://numpydoc.readthedocs.io/en/latest/format.html
+
+Variables
+---------
+This is where you mention any module-global variables.
+
+Functions
+---------
+This is where you can specify any functions and what they return.
+
+Notes
+-----
+Any other useful information.
+
+References
+----------
+If you are implementing complicated functionality that has associated references (papers, videos, presentations etc.)
+    leave links to them here.
+
+Examples
+--------
+Any useful examples of basic usage of this module, make sure to enclose code in 3 backtics, this is
+    interpreted by most modern IDE's as needing syntax highlighting which is quite useful. Here is an
+    example:
+    ```
+    import this
+    ```
+"""
+
+if __name__ == "__main__": # Code inside this statement will only run if the file is explicitly called and not just imported.
+    pass
